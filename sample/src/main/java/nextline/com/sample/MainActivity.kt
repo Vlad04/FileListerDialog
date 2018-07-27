e7ed166411af9b1439fb0623aef0bdee61a0e416
Vlad04 <vladiir.rod96@gmail.com>
2018-07-27 10:19:35 -0500

    Sample Done

---
+++ sample/src/main/java/nextline/com/sample/MainActivity.kt
@@ -0,0 +1,41 @@
+package nextline.com.sample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import kotlinx.android.synthetic.main.activity_main.*
+import yogesh.firzen.filelister.FileListerDialog
+import yogesh.firzen.filelister.OnFileSelectedListener
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button_library.setOnClickListener {
+            val fileListerDialog = FileListerDialog.createFileListerDialog(this@MainActivity, R.style.AppTheme)
+
+            fileListerDialog.setOnFileSelectedListener(object : OnFileSelectedListener {
+                override fun onFileSelected(file: File?, path: String?) {
+                    println("===============FILE " + file)
+                    println("===============PATH " + path)
+
+                }
+            })
+
+            fileListerDialog.setDefaultDir(Environment.getExternalStorageDirectory().path)
+
+            fileListerDialog.setFileFilter(FileListerDialog.FILE_FILTER.ALL_FILES)
+
+            fileListerDialog.show()
+
+            // /Tarjeta de memoria/HTC_MakeMoreSpace/Gallery
+
+        }
+
+    }
+}
